af5c6660c60bb56e436b43cbd1eae5b62eac0f2b
Ramis Cesar Damasceno <rdamasce@c1r2p3.42sp.org.br>
2025-07-21 21:57:53 -0300

    finish exercise ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdamasce <rdamasce@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 21:09:06 by rdamasce          #+#    #+#             */
+/*   Updated: 2025/07/21 21:55:31 by rdamasce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+#include <string.h>
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+
+	while (n != 0)
+	{
+		if (*(char *)s == c)
+		{
+			return((void *)s);
+		}
+		else
+		{
+			s++;
+			n--;
+		}
+	}
+	return(NULL);
+}
+
+/* int main(void)
+{
+	char buffer[] = "Hello, world";
+	char *ptr;
+
+	ptr = ft_memchr(buffer, 'w', strlen(buffer));
+
+	if (ptr != NULL) {
+		printf("Caractere 'w' encontrado em:  %s\n", ptr);
+	} else {
+		printf("Caractere 'w' nao encontrado. \n");
+	}
+
+	return (0);
+} */
