4198fc95214ce5f0950c65650c77f470182432e2
Ramis Cesar Damasceno <rdamasce@c3r2p1.42sp.org.br>
2025-07-29 21:46:50 -0300

    ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdamasce <rdamasce@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 21:04:42 by rdamasce          #+#    #+#             */
+/*   Updated: 2025/07/29 21:46:22 by rdamasce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int ft_atoi(char *str)
+{
+	unsigned int i;
+	int 	signal;
+	int res;
+
+	i = 0;
+	signal = 1;
+	res = 0;
+
+	if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+	{
+		i++;
+	}
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if(str[i] == '-')
+		{
+			signal = -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res *= 10;
+		res += str[i] - '0';
+		i++;
+	}
+	return (res * signal);
+}
+
+int main(void)
+{
+	char str1[] = "123";
+
+	int num1 = ft_atoi(str1);
+	printf("num1 = %d\n", num1);
+
+	return (0);
+}
