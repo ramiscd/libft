d673a5c145b24abfdf02aecd9721b4910e5f9c5d
Ramis Cesar Damasceno <rdamasce@c3r2p2.42sp.org.br>
2025-08-04 19:39:46 -0300

    add fuction ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,25 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: youvisa <youvisa@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rdamasce <rdamasce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 15:56:13 by youvisa           #+#    #+#             */
-/*   Updated: 2025/08/02 16:06:06 by youvisa          ###   ########.fr       */
+/*   Updated: 2025/08/04 19:38:15 by rdamasce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-  return (s1);
+	int		count1;
+	int		count2;
+	int		total;
+	int		i;
+	char	*join;
+	
+	
+	count1 = ft_strlen(s1);
+	count2 = ft_strlen(s2);
+	total = count1 + count2;
+	join = (char *)malloc((total + 1) * sizeof(char));
+	if (!join)
+		return (NULL);
+	else
+	{
+		i = 0;
+		while (s1[i] != '\0')
+		{
+			join[i] = s1[i];
+			i++;
+		}
+
+		count2 = 0;
+		while (i < total)
+		{
+			join[i] = s2[count2];
+			i++;
+			count2++;
+		}
+		return (join);
+	}
 }
 
-int main(void)
+/* int main(void)
 {
-  char s1[] = "Eu gosto de ";
-  char s2[] = "pao de queijo";
-  char *res = ft_strjoin(s1, s2);
-  printf("%s", res);
-  return (0);
-}
+	char s1[] = "Eu gosto de ";
+	char s2[] = "pao de queijo";
+	char *res = ft_strjoin(s1, s2);
+	printf("%s \n", res);
+	return (0);
+} */
